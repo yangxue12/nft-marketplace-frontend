d26376f24228dce516ba6d9a0bf4eb52f29851ef
yangxue12 <2325449412@qq.com>
2023-12-16 19:04:22 +0800

    2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ function Navbar({onConnectWallet,walletAddress}){
     return (
         <nav className="navbar">
             <div className="navbar-brand">NFT Marketplace</div>
+
             <div className="navbar-menu">
                 <button className="connect-wallet-button" onClick={onConnectWallet}>{walletAddress || "Wallet"}
                 </button>
